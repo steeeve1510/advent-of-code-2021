a0672c6c732a692385a3cb3ce52f439a322a7d0a
Stefan Heigl <stefan.heigl90@gmail.com>
2021-12-12 21:24:17 +0100

    solution day 2 part 2

--- src/main/java/com/google/Day2.java
+++ src/main/java/com/google/Day2.java
@@ -1,52 +1,42 @@
 package com.google;
 
 import com.google.util.ReadFileUtil;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Day2 {
 
     public static void main(String[] args) throws IOException {
-        var file = "/home/stefan/Desktop/advent-of-code/2/input";
+        var file = "/home/stefan/Desktop/advent-of-code/2/input2";
 
         List<DirectionChange> changes = ReadFileUtil.readLinesFrom(file).stream()
                 .map(DirectionChange::parse)
-                .collect(Collectors.toList());
+                .toList();
 
         long horizontal = 0;
         long depth = 0;
+        long aim = 0;
 
         for (var c : changes) {
-            switch (c.getDirection()) {
-                case up:
-                    depth -= c.getChange();
-                    break;
-                case down:
-                    depth += c.getChange();
-                    break;
-                case forward:
-                    horizontal += c.getChange();
-                    break;
+            switch (c.direction()) {
+                case up -> aim -= c.change();
+                case down -> aim += c.change();
+                case forward -> {
+                    horizontal += c.change();
+                    depth += aim * c.change();
+                }
             }
         }
 
         System.out.println(horizontal * depth);
     }
 
-    @RequiredArgsConstructor
-    @Getter
-    private static class DirectionChange {
-        private final Direction direction;
-        private final int change;
-
+    private record DirectionChange(Direction direction, int change) {
         public static DirectionChange parse(String text) {
             var parts = text.split(" ");
             var direction = Direction.valueOf(parts[0]);
-            var change = Integer.valueOf(parts[1]);
+            var change = Integer.parseInt(parts[1]);
             return new DirectionChange(direction, change);
         }
     }
