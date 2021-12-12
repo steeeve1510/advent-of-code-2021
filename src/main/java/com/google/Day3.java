523bdb25d5efe1ad3e9e5d8072bbb4401d81af68
Stefan Heigl <stefan.heigl90@gmail.com>
2021-12-12 22:27:36 +0100

    solution day 3 part 2

--- src/main/java/com/google/Day3.java
+++ src/main/java/com/google/Day3.java
@@ -3,43 +3,102 @@ package com.google;
 import com.google.util.ReadFileUtil;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Day3 {
 
+    private static int numberOfDigits = 12;
+
     public static void main(String[] args) throws IOException {
-        var file = "/home/stefan/Desktop/advent-of-code/3/input";
+        var file = "/home/stefan/Desktop/advent-of-code/3/input2";
 
-        List<String> number = ReadFileUtil.readLinesFrom(file);
+        List<String> lines = ReadFileUtil.readLinesFrom(file);
 
-        Map<Integer, Integer> occurrences = new HashMap<>();
-        long size = number.size();
+        List<BinaryNumber> numbers = lines.stream()
+                .map(BinaryNumber::parse)
+                .toList();
 
-        for (var n : number) {
-            var characters = n.toCharArray();
-            for (var i = 0; i < characters.length; i++) {
-                var c = characters[i];
-                var occurrence = occurrences.getOrDefault(i, 0);
-                if ('1' == c) {
-                    occurrence++;
-                }
-                occurrences.put(i, occurrence);
+        var oxygen = oxygen(numbers);
+        var co2 = co2(numbers);
+
+        System.out.println(oxygen * co2);
+    }
+
+    private static int oxygen(List<BinaryNumber> numbers) {
+        for (var i = 0; i < numberOfDigits; i++) {
+            var threshold = numbers.size() / 2f;
+            var count = count(numbers, i);
+            if (count >= threshold) {
+                numbers = filter(numbers, 1, i);
+            } else {
+                numbers = filter(numbers, 0, i);
+            }
+            if (numbers.size() <= 1) {
+                break;
             }
         }
+        if (numbers.size() > 1) {
+            throw new RuntimeException("Dafuq " + numbers.size());
+        }
+        var text = numbers.get(0).digits.stream()
+                .map(String::valueOf)
+                .reduce("", (a, b) -> a + b);
+        return Integer.parseInt(text, 2);
+    }
 
-        occurrences.forEach((k, v) -> {
-            if (v > size/2) {
-                System.out.println(k + ":1");
+    private static int co2(List<BinaryNumber> numbers) {
+        for (var i = 0; i < numberOfDigits; i++) {
+            var threshold = numbers.size()  / 2f;
+            var count = count(numbers, i);
+            if (count < threshold) {
+                numbers = filter(numbers, 1, i);
             } else {
-                System.out.println(k + ":0");
+                numbers = filter(numbers, 0, i);
             }
-        });
+            if (numbers.size() <= 1) {
+                break;
+            }
+        }
+        if (numbers.size() > 1) {
+            throw new RuntimeException("Dafuq " + numbers.size());
+        }
+        var text = numbers.get(0).digits.stream()
+                .map(String::valueOf)
+                .reduce("", (a, b) -> a + b);
+        return Integer.parseInt(text, 2);
+    }
 
-        // 111011110011 -> 3827
-        // 000100001100 -> 268
+    private static int count(List<BinaryNumber> numbers, int position) {
+        return numbers.stream()
+                .map(n -> n.get(position))
+                .reduce(0, Integer::sum);
+    }
 
-        System.out.println(3827 * 268);
+    private static List<BinaryNumber> filter(List<BinaryNumber> numbers, int criteria, int position) {
+        return numbers.stream()
+                .filter(n -> n.get(position) == criteria)
+                .toList();
+    }
+
+    private record BinaryNumber(List<Integer> digits) {
+
+        public Integer get(int index) {
+            return digits.get(index);
+        }
+
+        public static BinaryNumber parse(String text) {
+            var values = new ArrayList<Integer>();
+            var characters = text.toCharArray();
+            for (var c : characters) {
+                if ('0' == c) {
+                    values.add(0);
+                }
+                if ('1' == c) {
+                    values.add(1);
+                }
+            }
+            return new BinaryNumber(values);
+        }
     }
 }
